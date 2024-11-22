8faf89386161c65aadff21a839bcadaace959643
introvenko <volodymyry94@gmail.com>
2024-11-22 16:32:33 +0100

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2024 2Read App. All rights reserved.</p>
+        </footer>
+    );
+};
