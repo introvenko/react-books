b4eb0d59c4abce5efa974062d8cf29b8266e1231
introvenko <volodymyry94@gmail.com>
2024-11-22 17:34:15 +0100

    Believe in a miracle

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-books/',
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['react-star-rating-component']
-    }
-  }
+    outDir: 'dist',
+  },
 })
