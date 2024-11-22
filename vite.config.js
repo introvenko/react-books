37d45836d12d57242b5e5830d22f490e5f5fa6e0
introvenko <volodymyry94@gmail.com>
2024-11-22 17:57:27 +0100

    Commmmmmmit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-books/',
+  base: '/more-react-project/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-  },
-})
+  }
+});
